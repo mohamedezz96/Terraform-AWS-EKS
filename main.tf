6116f16fd260d3a2b6d88232cb09b6f0355d8a98
mohamed ezz <mohamed.elsayedezzeldin@vodafone.com>
2024-05-10 14:48:34 +0300

    adding node group

--- main.tf
+++ main.tf
@@ -21,4 +21,20 @@ module "eks-cluster" {
   subnet_ids = module.eks-network.eks_vpc_subnet_ids
 }
 
+module "eks-node-group" {
+  source = "github.com/mohamedezz96/Terraform-Modules/EKS/EKS-NodeGroup"
+
+  cluster_name = module.eks-cluster.cluster_name
+  node_group_name = "Group-A"
+  eks_vpc_private_subnet_ids = module.eks-network.eks_vpc_private_subnet_ids
+
+  node_group_ami_type = "BOTTLEROCKET_x86_64"
+  node_group_instance_type = ["t3.medium"]
+  node_group_capacity_type = "ON_DEMAND"
+
+  node_group_desired_size = 3
+  node_group_max_size = 5
+  node_group_min_size = 3
+  node_group_max_unavailable = 1
+}
 
